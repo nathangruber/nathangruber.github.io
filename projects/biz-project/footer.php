6925963286b5f50de2c11347c05ec2032ccf0d9d
nathangruber <grubernp@gmail.com>
2016-01-28 21:54:33 -0600

    added php files

---
+++ projects/biz-project/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <p><a href="file:///Users/ngruber/Dropbox/github/nathangruber.github.io/projects/biz-project/sitemap.html">Site Map</a></p>
+  </footer>
+<center>
+      <p><small>'&copy; 2016 Blake's Butcher Shop'</small></p>
+</center>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+   <script src="assets/js/bootstrap.min.js"></script>
---
+++ projects/biz-project/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+   <div class="container">
+     <div class="navbar-header">
+       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+         <span class="sr-only">Toggle navigation</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+       </button>
+       <a class="navbar-brand" href="index.html" id="black">Blake's Butcher Shop</a>
+     </div>
+     <div id="navbar" class="collapse navbar-collapse">
+       <ul class="nav navbar-nav">
+         <li><a href="index.html" class="black">Home</a></li>
+         <li><a href="contact.html" class="black">Contact</a></li>
+         <li><a href="products.html" class="black">Products</a></li> 
+         <li><a href="about.html" class="black">About</a></li>
+       </ul>
+     </div>
+   </div>
+ </nav>
